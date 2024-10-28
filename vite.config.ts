378761d8a5d06de1c64906cd9cda50d784494b38
Chris Martin <cjmartin@gmail.com>
2024-10-28 10:45:36 -0700

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/tahoe-robotics.github.io",
+  base: "/www.tahoerobotics.org",
   plugins: [react()],
 })
